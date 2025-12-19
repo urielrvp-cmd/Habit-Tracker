f89d1ab7dde09cb41629f1130570fbb808cdccd2
uriel <urielrvp@hmail.com>
2025-12-19 05:42:13 -0300

    useHabitos con estado inicial

--- Habit-Tracker/src/Components/Habitos/GestionHabitos/UseGestionHabitos.jsx
+++ Habit-Tracker/src/Components/Habitos/GestionHabitos/UseGestionHabitos.jsx
@@ -1,1 +1,9 @@
 //Manejara toda la logica y el estado de los habitos.
+
+import { useState } from "react";
+
+export function useHabitos() {
+  const [habitos, setHabitos] = useState([]);
+
+  return { habitos };
+}
