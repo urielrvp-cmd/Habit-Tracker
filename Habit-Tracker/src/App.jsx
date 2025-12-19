e59a85769ee2b17f4a3c397039126e986b3a0900
uriel <urielrvp@hmail.com>
2025-12-19 07:18:55 -0300

    mostrar lista de habitos

--- Habit-Tracker/src/App.jsx
+++ Habit-Tracker/src/App.jsx
@@ -1,13 +1,14 @@
 import { useHabitos } from "./components/Habitos/GestionHabitos/UseGestionHabitos";
 import FormularioHabito from "./Components/Habitos/FormularioNuevoHabitos/FormularioHabitos";
-
+import ListaHabitos from "./Components/Habitos/ListaDeHabitos/ListaHabitos"
 
 function App() {
-  const { agregarHabito } = useHabitos();
+  const { habitos, agregarHabito } = useHabitos();
   return (
     <div>
       <h1>Habit Tracker</h1>
       <FormularioHabito onAgregar={agregarHabito} />
+      <ListaHabitos habitos={habitos} />
     </div>
   );
 }
--- Habit-Tracker/src/Components/Habitos/ListaDeHabitos/ListaHabitos.jsx
+++ Habit-Tracker/src/Components/Habitos/ListaDeHabitos/ListaHabitos.jsx
@@ -1,7 +1,17 @@
 //Muestra la lista de habitos
 
-function ListaHabitos() {
-  return <div>Lista</div>;
+function ListaHabitos({ habitos }) {
+  if (habitos.length === 0) {
+    return <p>Sin hábitos</p>;
+  }
+
+  return (
+    <ul>
+      {habitos.map((habito) => (
+        <li key={habito.id}>{habito.nombre}</li>
+      ))}
+    </ul>
+  );
 }
 
 export default ListaHabitos;
